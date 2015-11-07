8f47b2f4b726715305e21f4ae661502df1d93c04
kotkarl <kotkarl@gmail.com>
2015-11-07 23:46:25 +0000

    base

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const api = require('./api');
+const mongoose = require('mongoose');
+const port = 4000;
+const app = express();
+app.use('/api', api);
+
+// Connect to mongodb
+mongoose.connect('localhost/app');
+mongoose.connection.once('open', () => {
+  console.log('Mongoose is connected');
+  app.listen(port, () => {
+    console.log('Server starting on port', port);
+  });
+});
