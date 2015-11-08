ab249f03aa0a516721c76aeb5345ad0361233f16
kotkarl <kotkarl@gmail.com>
2015-11-08 01:08:55 +0000

    setting up shiz

--- api.js
+++ api.js
@@ -9,17 +9,13 @@ const app = express();
 
 const ADMIN_TOKEN = 'dabs';
 
+/* A small string for 401 messages. */
+const msg401 = 'You are unauthorized to add a company.';
+
+/* A small string for 404 messages. */
+const msg404 = 'Company not found';
+
 /* HELPER FUNCTIONS */
-
-/* A small string concatonator for 401 messages. */
-function msg401(string) {
-  return 'You are unauthorized to add a ' + string + '.';
-}
-
-/* A small string concatonator for 404 messages. */
-function msg404(string) {
-  return _.capitalize(string + ' not found.');
-}
 
 /* Sends a pretty validation error message to the client
   for each validation error that occured. */
@@ -29,23 +25,6 @@ function handleValidationError(err, res) {
     msg = msg.concat(err.errors[key].message + '\n');
   });
   return res.status(412).send(msg);
-}
-
-/* Removes unwanted properties like 'token' and '__v'
-  from every user from a list of users. */
-function userRemoveUnwanted(users) {
-  const result = [];
-  for (let i = 0; i < users.length; i++) {
-    const temp = users[i];
-    const obj = {
-      _id: temp._id,
-      name: temp.name,
-      age: temp.age,
-      gender: temp.gender,
-    };
-    result.push(obj);
-  }
-  return result;
 }
 
 /* Validates the given id to prevent mongodb from sending
@@ -80,17 +59,17 @@ function companyRemoveUnwanted(companies) {
 /* COMPANY */
 
 /* Gets a list of all registered companies. */
-app.get('/company', (req, res) => {
+app.get('/companies', (req, res) => {
   models.Company.find({}, (err, companies) => {
     if(err) {
-      return res.status(500).send(err.message);
+      return res.status(500).send(err);
     }
     res.json(companyRemoveUnwanted(companies));
   });
 });
 
 /* Gets a single company with the given id. */
-app.get('/company/:id', (req, res) => {
+app.get('/companies/:id', (req, res) => {
   const id = req.params.id;
   validateId(id, (msg) => {
     if(msg) {
@@ -99,24 +78,37 @@ app.get('/company/:id', (req, res) => {
   });
   models.Company.findOne({'_id': id}, (err, company) => {
     if(err) {
-      return res.status(500).send(err.message);
+      return res.status(500).send(err);
     }
     if(!company) {
-      return res.status(404).send(msg404('company'));
+      return res.status(404).send(msg404);
     }
     res.json(companyRemoveUnwanted([company]));
   });
 });
 
 /* Registers a company. */
-app.post('/company', bodyParser.json(), (req, res) => {
+app.post('/companies', bodyParser.json(), (req, res) => {
   const adminToken = req.headers.admin_token;
-  // Admin authorization
+  const contentType = req.headers['content-type'];
+
+  // If ADMIN_TOKEN is missing or is incorrect, the server responds with status code 401
   if(!adminToken || adminToken !== ADMIN_TOKEN) {
-    return res.status(401).send(msg401('company'));
+    return res.status(401).send(msg401);
+  }
+  // If content type in the request header is not application/json, the server responds with status code 415
+  if(contentType !== 'application/json') {
+    return res.status(415).send('The \'content-type\' header must be \'application/json\'.');
   }
 
-  const c = new models.Company(req.body);
+  // If a company with the same name exists, the server responds with status code 409
+
+  //TODO check if a company name exists
+
+  //TODO if preconditions are met then the company is written to mongodb and elasticsearch.
+
+  return res.send('ok');
+  /* const c = new models.Company(req.body);
 
   c.save((err, company) => {
     if(err) {
@@ -125,97 +117,22 @@ app.post('/company', bodyParser.json(), (req, res) => {
     // Return the company ID to the client
     const companyId = {company_id: company._id};
     res.status(201).json(companyId);
-  });
+});*/
 });
 
-/* USER */
-
-/* Gets a list of all registered users */
-app.get('/user', (req, res) => {
-  models.User.find({}, (err, users) => {
-    if(err) {
-      return res.status(500).send(err.message);
-    }
-    // Remove token from each user and send result
-    res.json(userRemoveUnwanted(users));
-  });
+/* Updates a preexisting company. */
+app.post('/companies/:id', bodyParser.json(), (req, res) => {
+  // Update a given company
 });
 
-/* Registers a user to the database */
-app.post('/user', bodyParser.json(), (req, res) => {
-
-  const u = new models.User(req.body);
-
-  u.save((err, user) => {
-    if(err) {
-      return handleValidationError(err, res);
-    }
-    // Return the user to the client
-    res.status(201).json(user);
-  });
+/* Removes a previously added company. */
+app.delete('/companies/:id', (req, res) => {
+  // Remove a previously added company
 });
 
-/* PUNCHCARD */
-
-/* Creates a new punch card for the company with the given company_id
-  assigned to the user with the given token.*/
-app.post('/punchcard/:company_id', bodyParser.json(), (req, res) => {
-  const userToken = req.headers.token;
-
-  // Validate the token
-  models.User.findOne({'token': userToken}, (err, user) => {
-    if(err) {
-      return res.status(500).send(err.message);
-    }
-    if(!user) {
-      return res.status(401).send(msg401('punchcard'));
-    }
-    // Validate the company ID
-    const companyId = req.params.company_id;
-    validateId(companyId, (err) => {
-      if(err) {
-        return res.status(412).send(err);
-      }
-      // Check if the company exists
-      models.Company.findOne({'_id': companyId}, (err, company) => {
-        if(err) {
-          return res.status(500).send(err.message);
-        }
-        if(!company) {
-          return res.status(404).send(msg404('company'));
-        }
-        // If the punchCard has not expired, then the created date should be greater than 'punchExpireDate'
-        const msInADay = 86400000;
-        const punchExpireDate = _.now() - (company.punchcard_lifetime * msInADay);
-
-        // Check if the user already has a active punch card
-        models.Punchcard.findOne({'company_id': companyId, 'user_id': userToken,
-        'created': { $gt: punchExpireDate } }, (err, cardStillActive) => {
-          if(err) {
-            return res.status(500).send(err.message);
-          }
-          if(cardStillActive) {
-            return res.status(409).send('This user already has a working punch card for this company.');
-          }
-          // Validate the post data
-          const p = new models.Punchcard(
-            {
-              company_id: companyId,
-              user_id: userToken,
-              created: _.now(),
-            });
-
-          p.save((err, punchcard) => {
-            if(err) {
-              return handleValidationError(err, res);
-            }
-            // Return the punchcard ID to the client
-            res.status(201).json({'_id': punchcard._id});
-          });
-        });
-      });
-    });
-  });
+/* Used to search for a given company that has been added to Punchy. */
+app.post('/companies/search', bodyParser.json(), (req, res) => {
+  // Update a given company
 });
 
 module.exports = app;
