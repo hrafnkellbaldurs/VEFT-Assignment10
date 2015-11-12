0f740f70f1d7b0f6557ee612f4e859c5a0fdc283
kotkarl <kotkarl@gmail.com>
2015-11-12 06:13:05 +0000

    POST /companies/:id - 20 % Finished

--- api.js
+++ api.js
@@ -52,24 +52,65 @@ function validateId(id, cb) {
   cb(null);
 }
 
-/* Removes the '__v' property from every company
-  from a list of companies. */
-function companyRemoveUnwanted(companies) {
-  const result = [];
-  for (let i = 0; i < companies.length; i++) {
-    const temp = companies[i];
-    const obj = {
-      _id: temp._id,
-      name: temp.name,
-      description: temp.description,
-      punchcard_lifetime: temp.punchcard_lifetime,
-    };
-    result.push(obj);
-  }
-  return result;
+/* Finds the company with the given id and
+    and returns it through the callback function.
+    If there is an error, it also gets sent through
+    the callback function. */
+function getCompanyById(id, res, cb) {
+  validateId(id, (msg) => {
+    if(msg) {
+      res.status(412);
+      return cb(msg, res, null);
+    }
+    models.Company.findOne({'_id': id}, (err, company) => {
+      if(err) {
+        if(err.name === 'CastError') {
+          res.status(404);
+          return cb(MSG404, res, null);
+        }
+        res.status(500);
+        return cb(err, res, null);
+      }
+      if(!company) {
+        res.status(404);
+        return cb(MSG404, res, null);
+      }
+      return cb(null, null, company);
+    });
+  });
 }
 
-/* COMPANY */
+/* Checks the ADMIN_TOKEN header and Content-Type
+    header for errors. Sends an error message to the
+    callback function if there is an error. */
+function checkHeaders(req, res, cb) {
+  const adminToken = req.headers.admin_token;
+  const contentType = req.headers['content-type'];
+
+  // If ADMIN_TOKEN is missing or is incorrect, the server responds with status code 401
+  if(!adminToken || adminToken !== ADMIN_TOKEN) {
+    res.status(401);
+    return cb(MSG401, res);
+  }
+  // If content type in the request header is not [aA]pplication/json, the server responds with status code 415
+  if(_.capitalize(contentType) !== 'Application/json') {
+    res.status(415);
+    return cb('The \'content-type\' header must be \'[aA]pplication/json\'.', res);
+  }
+
+  return cb(null);
+}
+
+/* Returns a company with wanted variables */
+function excludeTitles(company) {
+  return {
+    title: company.title,
+    description: company.description,
+    url: company.url,
+  };
+}
+
+/* API METHODS */
 
 /* Gets a list of all registered companies. */
 app.get('/companies', (req, res) => {
@@ -120,105 +161,135 @@ app.get('/companies', (req, res) => {
 /* Gets a single company with the given id. */
 app.get('/companies/:id', (req, res) => {
   const id = req.params.id;
-  validateId(id, (msg) => {
-    if(msg) {
-      return res.status(412).send(msg);
+  getCompanyById(id, res, (err, resErr, company) => {
+    if(err) {
+      return resErr.send(err);
     }
-    models.Company.findOne({'_id': id}, (err, company) => {
-      if(err) {
-        if(err.name === 'CastError') {
-          return res.status(404).send(MSG404);
-        }
-        return res.status(500).send(err);
-      }
-      if(!company) {
-        return res.status(404).send(MSG404);
-      }
-      res.json({
-        id: company._id,
-        title: company.title,
-        description: company.description,
-        url: company.url,
-      });
-    });
+    const foundCompany = {
+      id: company._id,
+      title: company.title,
+      description: company.description,
+      url: company.url,
+    };
+    res.send(foundCompany);
   });
 });
 
 /* Registers a company. */
 app.post('/companies', bodyParser.json(), (req, res) => {
-  const adminToken = req.headers.admin_token;
-  const contentType = req.headers['content-type'];
+  /* Check if the user is authorized and has the
+      correct type of content */
+  checkHeaders(req, res, (err, resErr) => {
 
-  // If ADMIN_TOKEN is missing or is incorrect, the server responds with status code 401
-  if(!adminToken || adminToken !== ADMIN_TOKEN) {
-    return res.status(401).send(MSG401);
-  }
-  // If content type in the request header is not [aA]pplication/json, the server responds with status code 415
-  if(_.capitalize(contentType) !== 'Application/json') {
-    return res.status(415).send('The \'content-type\' header must be \'application/json\'.');
-  }
-
-  const company = req.body;
-
-  if(company.title === null) {
-    return res.status(412).send('Company title cannot be null.');
-  }
-
-  // If a company with the same name exists, the server responds with status code 409
-  models.Company.findOne({'title': company.title}, (err, doc) => {
     if(err) {
-      return res.status(500).send(err);
-    }
-    if(doc) {
-      return res.status(409).send(MSG409);
+      return resErr.send(err);
     }
 
-    // If all is well, save into the database
-    const c = new models.Company(
-      {
-        title: company.title,
-        description: company.description || '',
-        url: company.url || '',
-        created: new Date(),
-      }
-    );
+    const company = req.body;
 
-    c.save((err, dbCompany) => {
+    /* If a company with the same name exists,
+      the server responds with status code 409 */
+    models.Company.findOne({'title': company.title}, (err, doc) => {
       if(err) {
-        return handleValidationError(err, res);
+        return res.status(500).send(err);
+      }
+      if(doc) {
+        return res.status(409).send(MSG409);
       }
 
-      const companyId = dbCompany._id.toString();
+      // If all is well, save into the database
+      const c = new models.Company(
+        {
+          title: company.title,
+          description: company.description || '',
+          url: company.url || '',
+          created: new Date(),
+        }
+      );
 
-      // If the company is saved to the database, index it in ES
-      const data = {
-        title: dbCompany.title,
-        description: dbCompany.description,
-        url: dbCompany.url,
-        created: dbCompany.created,
-      };
+      c.save((err, dbCompany) => {
+        if(err) {
+          return handleValidationError(err, res);
+        }
 
-      const promise = client.index({
-        index: 'companies',
-        type: 'company',
-        id: companyId,
-        body: data,
-      });
+        const companyId = dbCompany._id.toString();
 
-      promise.then((doc) => {
-        // Return the company ID to the client
-        res.status(201).json({id: companyId});
-      }, (err) => {
-        res.status(500).send(err);
+        // If the company is saved to the database, index it in ES
+        const data = excludeTitles(dbCompany);
+        data.created = dbCompany.created;
+
+        const promise = client.index({
+          index: 'companies',
+          type: 'company',
+          id: companyId,
+          body: data,
+        });
+
+        promise.then((doc) => {
+          // Return the company ID to the client
+          res.status(201).json({id: companyId});
+        }, (err) => {
+          res.status(500).send(err);
+        });
       });
     });
   });
 });
 
-/* Updates a preexisting company.
-    TODO finish this */
+/* Updates a preexisting company. */
 app.post('/companies/:id', bodyParser.json(), (req, res) => {
-  // Update a given company
+  /* Check if the user is authorized and has the
+      correct type of content */
+  checkHeaders(req, res, (err, resErr) => {
+    if(err) {
+      return resErr.send(err);
+    }
+
+    // Check if the id matches with a registered company
+    const id = req.params.id;
+    getCompanyById(id, res, (err, resErr, company) => {
+      if(err) {
+        return resErr.send(err);
+      }
+
+      /* If it matches, update the company with the
+          updated information. */
+      const newCompany = req.body;
+      if(newCompany.title) {
+        company.title = newCompany.title;
+      }
+
+      if(newCompany.description) {
+        company.description = newCompany.description;
+      }
+
+      if(newCompany.url) {
+        company.url = newCompany.url;
+      }
+
+      // Save the changes to the database
+      company.save((err, dbCompany) => {
+        if(err) {
+          return res.status(500).send(err);
+        }
+
+        // If all is well, reindex ElasticSearch
+        const promise = client.index({
+          index: 'companies',
+          type: 'company',
+          id: id,
+          body: excludeTitles(dbCompany),
+        });
+
+        // Return the result to the client
+        promise.then((doc) => {
+          res.send('The company \'' + company.title + '\' has been successfully edited.');
+        }, (err) => {
+          res.status(500).send(err);
+        });
+      });
+    });
+  });
 });
 
 /* Removes a previously added company.
